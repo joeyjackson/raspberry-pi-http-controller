b0b1bdc39d2d09a33a73e9ccb634c06ed0f23372
joeyjackson <joeyjackson4712@gmail.com>
2022-09-29 23:53:33 -0700

    sqlite database for user persistance

--- server/app.py
+++ server/app.py
@@ -3,6 +3,9 @@ import os
 import logging
 from threading import Thread, Lock
 
+from sqlalchemy import Column, ForeignKey, Boolean, String, create_engine, select
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+
 from flask import Flask, request, render_template
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -27,32 +30,90 @@ logger = logging.getLogger("controller")
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                  DATABASE                                   #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+Base = declarative_base()
+
+class UserRole(Base):
+  __tablename__ = "user_role"
+  username = Column(ForeignKey("user.username"), primary_key=True)
+  role_name = Column(ForeignKey("role.name"), primary_key=True)
+  active = Column(Boolean)
+  user = relationship("User", back_populates="roles")
+  role = relationship("Role", back_populates="users")
+
+class User(Base):
+  __tablename__ = "user"
+  
+  username = Column(String, primary_key=True)
+  password_hash = Column(String)
+  
+  roles = relationship("UserRole", back_populates="user")
+  
+  def __repr__(self):
+    return f"User(username={self.username!r})"
+
+
+class Role(Base):
+  __tablename__ = "role"
+  
+  name = Column(String, primary_key=True)
+  
+  users = relationship("UserRole", back_populates="role")
+  
+  def __repr__(self):
+    return f"Role(name={self.name!r})"
+
+# Create admin user on first startup
+db_filepath = os.getenv("DB_FILE_PATH", "db.sqlite")
+engine = create_engine(f"sqlite:///{db_filepath}", future=True)
+Session = sessionmaker(engine)
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #                                  AUTH                                       #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+ADMIN_ROLE_NAME = "admin"
+
+def setup_db():
+  should_create_admin = not os.path.isfile(db_filepath)
+
+  Base.metadata.create_all(engine)
+
+  if should_create_admin:
+    admin_username = os.getenv("ADMIN_USER", "admin")
+    admin_pwd = os.getenv("ADMIN_PASSWORD")
+    if not admin_pwd:
+      raise Exception("Must specify ADMIN_PASSWORD")
+    logger.info("Database not detected, creating with admin user (%s) at %s", admin_username, db_filepath)
+    with Session() as session:
+      admin_user = User(username=admin_username, password_hash=generate_password_hash(admin_pwd))
+      admin_role = Role(name=ADMIN_ROLE_NAME)
+      link = UserRole(active=True, role=admin_role, user=admin_user)
+      session.add_all([admin_role, admin_user, link])
+      session.commit()
 
 # https://flask-httpauth.readthedocs.io/en/latest/
 auth = HTTPBasicAuth()
 
-admin_username = os.getenv("ADMIN_USER", "admin")
-admin_pwd = os.getenv("ADMIN_PASSWORD")
-if not admin_pwd:
-  raise Exception("Must specify ADMIN_PASSWORD")
-
-users = {
-  admin_username: generate_password_hash(admin_pwd),
-}
+@auth.verify_password
+def verify_password(username, password):
+  with Session() as session:
+    user = session.get(User, username)
+    if user and check_password_hash(user.password_hash, password):
+      return user.username
 
 @auth.get_user_roles
 def get_user_roles(user):
-  if user == "admin":
-    return ["admin"]
-  else:
-    return []
-
-@auth.verify_password
-def verify_password(username, password):
-  if username in users and check_password_hash(users.get(username), password):
-    return username
+  with Session() as session:
+    stmt = select(UserRole.role_name, UserRole.active).where(UserRole.username == user)
+    roles = []
+    for role, active in session.execute(stmt):
+      if active:
+        roles.append(role)
+    return roles
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -96,7 +157,7 @@ def index():
   return render_template("index.html", current_user=auth.current_user())
 
 @app.post("/api/led")
-@auth.login_required(role=["admin"])
+@auth.login_required(role=[ADMIN_ROLE_NAME])
 def led_ctl():
   content_type = request.headers.get('Content-Type')
   if (content_type == 'application/json'):
@@ -114,6 +175,7 @@ def led_ctl():
 
 if __name__ == "__main__":
   try:
+    setup_db()
     setup_gpio()
     app.run(host="0.0.0.0")
   finally:
